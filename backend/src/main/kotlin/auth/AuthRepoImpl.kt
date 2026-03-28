9748f6d9fd8b1fcfc46e2cea2fe7d04935d1a07c
ManuelAcMn <marcmoy@iesmartinezm.es>
2026-03-28 20:45:20 +0100

    fix(backend): Manejo de excepciones relacionadas con la autenticacion

--- backend/src/main/kotlin/auth/AuthRepoImpl.kt
+++ backend/src/main/kotlin/auth/AuthRepoImpl.kt
@@ -3,42 +3,53 @@ package com.astrais.auth
 import com.astrais.db.DatabaseDAO
 import com.astrais.db.getDatabaseDaoImpl
 import kotlinx.datetime.toKotlinLocalDate
+import org.jetbrains.exposed.v1.exceptions.ExposedSQLException
 import org.slf4j.LoggerFactory
 
 private val log = LoggerFactory.getLogger(AuthRepoImpl::class.java)
 
 class AuthRepoImpl : AuthRepo{
     override suspend fun performBasicLogin(loginRequest: LoginRequest) : LoginResponse?{
-        val user = getDatabaseDaoImpl().getUsuario(loginRequest.email)
-        if (user?.contrasenia != null && checkPassword(loginRequest.passwd, user.contrasenia!!)){
-            getDatabaseDaoImpl().setUserLastLogin(user)
-            log.debug("Usuario ${user.nombre} conectado, Generando tokens...")
-            // Genera JWT
-            return LoginResponse(jwtAccessToken = generateAccessToken(user), jwtRefreshToken = generateRefreshToken(user))
-        }else{
-            return null
+        try {
+            val user = getDatabaseDaoImpl().getUsuario(loginRequest.email)
+            if (user?.contrasenia != null && checkPassword(loginRequest.passwd, user.contrasenia!!)){
+                getDatabaseDaoImpl().setUserLastLogin(user)
+                log.debug("Usuario ${user.nombre} conectado, Generando tokens...")
+                // Genera JWT
+                return LoginResponse(jwtAccessToken = generateAccessToken(user), jwtRefreshToken = generateRefreshToken(user))
+            }
+        } catch (e : ExposedSQLException){
+            log.error("Error trying to log user ${loginRequest.email}! Message: ${e.message}")
         }
+        return null
     }
 
     override suspend fun performBasicRegister(registerRequest: RegisterRequest) : Boolean{
-        val dao : DatabaseDAO = getDatabaseDaoImpl()
-        val existeUser = getDatabaseDaoImpl().getUsuario(registerRequest.email) != null
+        try {
+            val dao : DatabaseDAO = getDatabaseDaoImpl()
+            val existeUser = getDatabaseDaoImpl().getUsuario(registerRequest.email) != null
 
-        if (!existeUser){
-            val hashContrasenia = hashPassword(registerRequest.passwd)
-            dao.createUser(registerRequest.name, registerRequest.email, hashContrasenia, registerRequest.lang)
-            log.debug("Usuario ${registerRequest.name} registrado")
-            return true
+            if (!existeUser){
+                val hashContrasenia = hashPassword(registerRequest.passwd)
+                dao.createUser(registerRequest.name, registerRequest.email, hashContrasenia, registerRequest.lang)
+                log.debug("Usuario ${registerRequest.name} registrado")
+                return true
+            }
+        } catch (e : ExposedSQLException){
+            log.error("Error trying to register user ${registerRequest.name} with mail ${registerRequest.email}! Message: ${e.message}")
         }
         return false
     }
 
     override suspend fun regenAccessToken(id: Int): String? {
-        val user = getDatabaseDaoImpl().getUsuarioByID(id)
-        if (user != null){
-            return generateAccessToken(user)
-        }else{
-            return null
+        try {
+            val user = getDatabaseDaoImpl().getUsuarioByID(id)
+            if (user != null){
+                return generateAccessToken(user)
+            }
+        } catch (e : ExposedSQLException){
+            log.error("Error trying regen access token for $id! Message: ${e.message}")
         }
+        return null
     }
 }
