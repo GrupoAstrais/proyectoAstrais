46a5e054ce1d24fd93bc12bc7e0914c6a2a985af
ManuelAcMn <marcmoy@iesmartinezm.es>
2026-04-14 00:08:07 +0200

    feat(backend): Canal SSE por usuario

--- backend/src/main/kotlin/SSE.kt
+++ backend/src/main/kotlin/SSE.kt
@@ -1,24 +1,49 @@
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.routing.*
 import io.ktor.server.sse.*
 import io.ktor.sse.*
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.util.concurrent.ConcurrentHashMap
 
-const val SSEEVENT_RELOADSTORE = "RELOAD.STORE"
+
+enum class SSEEventType(val value : String) {
+    /** Evento global de recarga de tienda */
+    RELOAD_STORE("RELOAD.STORE"),
+    /** Evento por persona de agregar tarea */
+    ADD_TASK("ADDED.TASK")
+    ;
+
+    companion object {
+        fun fromValue(v: String) : SSEEventType? = entries.find { it.value.equals(v, ignoreCase = true) }
+    }
+}
+
+@Serializable
+data class SSEEventAddTaskData(
+    val gid : Int,
+    val tid : Int
+)
 
 /**
  * Mensaje SSE que se puede enviar
  */
 data class SSEMessage(
     /** El identificador del evento SSE */
-    val event: String,
+    val event: SSEEventType,
     /** Los datos del evento SSE */
     val data: String
 )
 
+const val GLOBAL_BUFFER_CAPACITY = 10
+const val USER_BUFFER_CAPACITY = 10
+
 object GlobalBusSSE {
-    private val _events = MutableSharedFlow<SSEMessage>()
+    private val _events = MutableSharedFlow<SSEMessage>(extraBufferCapacity = GLOBAL_BUFFER_CAPACITY)
     /** Sharedflow para los mensajes SSE que se envian */
     public val events : SharedFlow<SSEMessage> = _events
 
@@ -33,8 +58,35 @@ object GlobalBusSSE {
     /**
      * Shortcut para enviar eventos de recargado de tienda
      */
-    suspend fun publishStoreReload() = publish(SSEMessage(event = SSEEVENT_RELOADSTORE, data = ""))
+    suspend fun publishStoreReload() = publish(SSEMessage(event = SSEEventType.RELOAD_STORE, data = ""))
 }
+
+object UserBusSSE {
+    private val _events = ConcurrentHashMap<Int, MutableSharedFlow<SSEMessage>>()
+
+    public fun subscribeChannel(uid : Int) : MutableSharedFlow<SSEMessage>{
+        // A veces amo kotlin, esto es una maldita linea
+        _events.putIfAbsent(uid, MutableSharedFlow(extraBufferCapacity = USER_BUFFER_CAPACITY))
+        return _events[uid]!!
+    }
+
+    public fun unsubscribeChannel(uid: Int) {
+        _events.remove(uid)
+    }
+
+    public suspend fun publish(uid: Int, message: SSEMessage) {
+        _events[uid]?.emit(message)
+    }
+
+    public suspend fun publishAddTask(uid : Int, data : SSEEventAddTaskData) = publish(
+        uid = uid,
+        message = SSEMessage(
+            event = SSEEventType.ADD_TASK,
+            data = Json.encodeToString(data)
+        )
+    )
+}
+
 
 fun Application.installSSE(){
     install(SSE)
@@ -46,9 +98,32 @@ fun Route.sseRoutes() {
             send(
                 ServerSentEvent(
                     data = msg.data,
-                    event = msg.event
+                    event = msg.event.value
                 )
             )
+        }
+    }
+    authenticate("access-jwt") {
+        sse("/events/user") {
+            val token = call.principal<JWTPrincipal>()
+            val uid = token?.subject?.toInt() ?: -1
+            if (uid == -1){
+                return@sse
+            }
+
+            val flow = UserBusSSE.subscribeChannel(uid)
+            try {
+                flow.collect { msg->
+                    send(
+                        ServerSentEvent(
+                            data = msg.data,
+                            event = msg.event.value
+                        )
+                    )
+                }
+            } finally {
+                UserBusSSE.unsubscribeChannel(uid)
+            }
         }
     }
 }
